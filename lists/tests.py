5817217122077608dccb8ede37099f056e02ece9
Lelio Campanile <lelio.campanile@gmail.com>
2015-12-30 15:19:30 +0100

    prima vista e test

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,22 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
+        # test che controlla esistenza e corrispondenza tra url e vista
+
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_correct_html(self):
+        # test della vista; controlla che la vista restituisce una pag html
+
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
