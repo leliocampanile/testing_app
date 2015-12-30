17038d61ff220226abd32073a9ca716efae4dd6f
Lelio Campanile <lelio.campanile@gmail.com>
2015-12-30 15:58:03 +0100

    aggiunti functional test

--- functional_tests.py
+++ functional_tests.py
@@ -19,18 +19,27 @@ class NewVisitorTest(unittest.TestCase):
 
         # test il title della pagina
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         self.fail('Finish test!')
 
         # descriviamo nei commenti i test che dobbiamo ancora implementare
         # che rappresentano la nostra story
 
         # invitiamo ad inserire un item To-Do
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # digita "scrivo qualcosa" in una text box
+        inputbox.send_keys('scrivo qualcosa')
 
         # una volta premuto enter la pagina si aggiorna ed ora la page list
         # diventa: "1: scrivo qualcosa" è un item della To-Do list
+        inputbox.send_keys(Keys.ENTER)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: scrivo qualcosa' for row in rows))
         # la textbox è ancora presente per inserire alri item
         # scriviamo nella text box "un altro item"
 
